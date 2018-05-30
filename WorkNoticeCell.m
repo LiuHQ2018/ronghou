7cb3559fe6533909dcc227bbc978d7e368c3c43b
LiuHQ2018 <543416432@qq.com>
2018-05-30 15:53:50 +0800

    测试一下

---
+++ WorkNoticeCell.m
@@ -0,0 +1,24 @@
+//
+//  WorkNoticeCell.m
+//  NewBYOD
+//
+//  Created by LiuHaiQiang on 2017/11/23.
+//  Copyright © 2017年 LiuHaiQiang. All rights reserved.
+//
+
+#import "WorkNoticeCell.h"
+
+@implementation WorkNoticeCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
